c3ca977b21ff43929e887bdfe31fbf93f9063095
celeri <celestin.bouchet@gmail.com>
2023-11-05 08:56:17 -0500

    Boutons supprimer

--- src/lib/components/Module.svelte
+++ src/lib/components/Module.svelte
@@ -2,27 +2,42 @@
 	import AddNewModule from '$lib/components/AddNewModule.svelte';
 	import TitleModule from '$lib/components/TitleModule.svelte';
 	import Section from '$lib/components/Section.svelte';
+	import Icon from '@iconify/svelte';
 
 	export let moduleType: string;
 
 	//Ugly way to disconnect module and its children from page
 	const endModule = moduleType;
 
-	let dynamicComponents: any[] = [TitleModule, AddNewModule];
+	let dynamicComponents: { id: number; element: any }[] = [];
 
 	async function addDynamicComponent() {
-		dynamicComponents = [...dynamicComponents, Section];
+		dynamicComponents = [...dynamicComponents, { id: dynamicComponents.length, element: Section }];
+	}
+
+	function removeDynamicComponent(id: number) {
+		dynamicComponents = [
+			...dynamicComponents.filter((dynamicComponent) => dynamicComponent.id !== id)
+		];
 	}
 </script>
 
 <h1>{endModule}</h1>
 
-{#each dynamicComponents as element}
-	{#if element === AddNewModule}
-		<AddNewModule on:click={() => addDynamicComponent()}>Ajouter une section</AddNewModule>
-	{:else if element === Section}
+<TitleModule />
+<AddNewModule on:click={() => addDynamicComponent()}>Ajouter une section</AddNewModule>
+{#each dynamicComponents as dynamicComponent (dynamicComponent.id)}
+	{#if dynamicComponent.element === Section}
 		<Section moduleType={endModule} />
 	{:else}
-		<svelte:component this={element} />
+		<svelte:component this={dynamicComponent.element} />
 	{/if}
+	<div class="print:hidden">
+		<button
+			class="btn-icon variant-outline-error hover:variant-filled-error"
+			on:click={() => removeDynamicComponent(dynamicComponent.id)}
+		>
+			<Icon icon="ph:x" />
+		</button>
+	</div>
 {/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+
 	import AddNewModule from '$lib/components/AddNewModule.svelte';
 	import Module from '$lib/components/Module.svelte';
 	import LeftWindow from '$lib/components/LeftWindow.svelte';
 
-	let dynamicComponents: any[] = [AddNewModule];
+	let dynamicComponents: { id: number; element: any }[] = [];
 	let selectedType = '';
 
-	async function addDynamicComponent() {
+	function addDynamicComponent() {
 		if (selectedType != '') {
-			dynamicComponents = [...dynamicComponents, Module];
+			dynamicComponents = [...dynamicComponents, { id: dynamicComponents.length, element: Module }];
 		}
+	}
+
+	function removeDynamicComponent(id: number) {
+		dynamicComponents = [
+			...dynamicComponents.filter((dynamicComponent) => dynamicComponent.id !== id)
+		];
 	}
 </script>
 
@@ -20,18 +28,25 @@
 	<div
 		class="h-full col-span-2 print:col-span-4 rounded-sm bg-surface-100 text-surface-900 p-5 text-center"
 	>
-		{#each dynamicComponents as element}
-			{#if element === AddNewModule}
-				<AddNewModule on:click={() => addDynamicComponent()}>Ajouter un module</AddNewModule>
-			{:else if element === Module}
+		<AddNewModule on:click={() => addDynamicComponent()}>Ajouter un module</AddNewModule>
+		{#each dynamicComponents as dynamicComponent (dynamicComponent.id)}
+			{#if dynamicComponent.element === Module}
 				<div
 					class="border-solid border-2 border-sky-500 p-8 m-4 space-y-4 print:border-none print:p-0"
 				>
 					<Module moduleType={selectedType} />
 				</div>
 			{:else}
-				<svelte:component this={element} />
+				<svelte:component this={dynamicComponent.element} />
 			{/if}
+			<div class="print:hidden">
+				<button
+					class="btn-icon variant-outline-error hover:variant-filled-error"
+					on:click={() => removeDynamicComponent(dynamicComponent.id)}
+				>
+					<Icon icon="ph:x" />
+				</button>
+			</div>
 		{/each}
 	</div>
 	<div class="bg-surface-600 rounded-l-lg h-full p-5 print:hidden">
