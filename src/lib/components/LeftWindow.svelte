6c26b8e9cb3cafb6a10fec2cdbe90fea468f291b
celeri <celestin.bouchet@gmail.com>
2023-11-05 08:28:22 -0500

    Plus de style

--- src/lib/components/LeftWindow.svelte
+++ src/lib/components/LeftWindow.svelte
@@ -1,51 +1,53 @@
 <script lang="ts">
+	export let selectedOption = '';
+	let selectedRadio: number | null = null;
+	let headerChecked = false;
+	let footerChecked = false;
 
-export let selectedOption = '';
-let selectedRadio: number | null = null;
-let headerChecked = false;
-let footerChecked = false;
+	const radioButtons = [
+		{ id: 1, label: 'Titre' },
+		{ id: 2, label: 'Définition' },
+		{ id: 3, label: 'Matériel' },
+		{ id: 4, label: 'Etape' },
+		{ id: 5, label: 'Résultat' },
+		{ id: 6, label: 'Signatures' }
+	];
 
-    const radioButtons = [
-    { id: 1, label: 'Titre' },
-    { id: 2, label: 'Définition' },
-    { id: 3, label: 'Matériel' },
-    { id: 4, label: 'Etape' },
-    { id: 5, label: 'Résultat' },
-    { id: 6, label: 'Signatures' },
-  ];
-
-        
-        function handleRadioButtonChange(event: any) {
-    selectedRadio = parseInt(event.target.value, 10);
-    selectedOption = radioButtons[selectedRadio-1].label;
-  }
-
+	function handleRadioButtonChange(event: any) {
+		selectedRadio = parseInt(event.target.value, 10);
+		selectedOption = radioButtons[selectedRadio - 1].label;
+	}
 </script>
 
 <div>
-    <h1>Choisissez un module à ajouter</h1>
+	<h1>Choisissez un module à ajouter</h1>
 
-    {#each radioButtons as radioButton}
-  <label>
-    <input
-      type="radio"
-      name="radios"
-      bind:group={selectedRadio}
-      value={radioButton.id}
-      on:change={handleRadioButtonChange}
-    />
-    {radioButton.label}
-  </label>
-{/each}
+	<div class="space-y-2 my-5">
+		{#each radioButtons as radioButton}
+			<label class="flex items-center space-x-2">
+				<input
+					class="radio rounded-full"
+					type="radio"
+					name="radios"
+					bind:group={selectedRadio}
+					value={radioButton.id}
+					on:change={handleRadioButtonChange}
+				/>
+				<p>{radioButton.label}</p>
+			</label>
+		{/each}
 
-    <label>
-        <input type="checkbox" bind:checked={headerChecked} />
-        Header
-      </label>
-      <label>
-        <input type="checkbox" bind:checked={footerChecked} />
-        Footer
-      </label>
+		<label class="flex items-center space-x-2">
+			<input class="checkbox" type="checkbox" bind:checked={headerChecked} />
+			<p>Header</p>
+		</label>
+		<label class="flex items-center space-x-2">
+			<input class="checkbox" type="checkbox" bind:checked={footerChecked} />
+			<p>Footer</p>
+		</label>
+	</div>
 
-      <button on:click={()=> print()}>Imprimer</button>
+	<button class="btn variant-outline hover:variant-filled" on:click={() => print()}>
+		Imprimer
+	</button>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,13 +14,19 @@
 </script>
 
 <div class="grid grid-cols-4 gap-5 min-h-[100vh] py-5">
-	<div class="h-full print:hidden"><LeftWindow bind:selectedOption={selectedType} /></div>
-	<div class="h-full col-span-2 print:col-span-4 rounded-sm bg-surface-100 text-surface-900 p-2 text-center">
+	<div class="bg-surface-600 rounded-r-lg h-full p-2 print:hidden">
+		<LeftWindow bind:selectedOption={selectedType} />
+	</div>
+	<div
+		class="h-full col-span-2 print:col-span-4 rounded-sm bg-surface-100 text-surface-900 p-5 text-center"
+	>
 		{#each dynamicComponents as element}
 			{#if element === AddNewModule}
 				<AddNewModule on:click={() => addDynamicComponent()}>Ajouter un module</AddNewModule>
 			{:else if element === Module}
-				<div class="border-solid border-2 border-sky-500 p-8 m-4 space-y-4">
+				<div
+					class="border-solid border-2 border-sky-500 p-8 m-4 space-y-4 print:border-none print:p-0"
+				>
 					<Module moduleType={selectedType} />
 				</div>
 			{:else}
@@ -28,7 +34,7 @@
 			{/if}
 		{/each}
 	</div>
-	<div class="bg-surface-500 rounded-l-lg h-full p-2 print:hidden">
+	<div class="bg-surface-600 rounded-l-lg h-full p-5 print:hidden">
 		Colonne 3 pour changer le style mais on a pas eu le temps :D
 	</div>
 </div>
